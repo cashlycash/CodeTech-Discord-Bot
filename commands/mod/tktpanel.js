546979108f93b48fcaa89f85300149acf82f78c3
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2023-10-21 22:04:54 +0530

    updated dsc pkg ver + added cmds for tkt panel and verify panel

---
+++ commands/mod/tktpanel.js
@@ -0,0 +1,31 @@
+const { MessageActionRow, MessageEmbed, MessageButton } = require("discord.js");
+
+module.exports = {
+  name: "tktpanel",
+  description: "Make a ticket panel",
+  run: async (client, message) => {
+
+    if (!message.member.permissions.has('MANAGE_GUILD')){
+      return;
+    }
+
+    ch = message.channel
+
+    const emb = new MessageEmbed()
+      .setTitle('New Ticket')
+      .setDescription('Click on the button below to create a ticket')
+      .addField('NOTE!', '**`Spaming of ticket will result in strict action`**')
+      .setColor('BLURPLE')
+
+    const btn = new MessageActionRow()
+      .setComponents(
+        new MessageButton() 
+          .setLabel('CREATE TICKET')
+          .setStyle('PRIMARY')
+          .setEmoji('🎫')
+          .setCustomId('tkt')
+      )
+    ch.send({embeds: [emb], components: [btn]})
+    message.delete();
+  },
+};
---
+++ commands/mod/verifypanel.js
@@ -0,0 +1,28 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const owners = ["908554250945183744"];
+
+module.exports = {
+  name: "verifypanel",
+  run: (client, message, args) => {
+
+    if (!message.member.permissions.has('MANAGE_GUILD')){
+      return;
+    }
+    
+    const emb = new MessageEmbed()
+      .setTitle("Verify")
+      .setColor("#6600ff")
+      .setDescription(
+        "Click the button bellow to verify and get access to the rest of the server"
+      );
+
+    const btn = new MessageActionRow().setComponents(
+      new MessageButton()
+        .setLabel("Verify")
+        .setCustomId("verify")
+        .setStyle("SUCCESS")
+    );
+    message.channel.send({ embeds: [emb], components: [btn] });
+    message.delete();
+  },
+};
