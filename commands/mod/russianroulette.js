0240de448df52c37028dc43f764ea95bead754de
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2023-10-23 18:11:10 +0530

    Minor bug fixes

--- commands/mod/russianroulette.js
+++ commands/mod/russianroulette.js
@@ -2,35 +2,37 @@ const ms = require("ms");
 
 module.exports = {
   name: "russianroulette",
-  aliases: ["gol", "roulette", "grr"],
+  aliases: ["gol", "roulette", "grr", "god"],
   description: "Play russian roulette where the unlucky one gets muted",
   usage: "<time> <@user> <@user> ...",
   UserPerms: ["ADMINISTRATOR"],
   run: async (client, message, args) => {
     var pps = message.mentions.members;
     var pps  = pps.toJSON();
-    var unlucky = pps[Math.floor(Math.random() * array.length)];
+    var unlucky = pps[Math.floor(Math.random() * pps.length)];
     var newpps = pps.filter((x) => x.id !== unlucky.id);
-    var newpps = newpps.push(unlucky);
+    newpps.push(unlucky);
+
+    if (newpps.length < 2) return message.reply(`Not enough players`);
 
     message.reply(`Welcome to russian roulette\n\`[ Spinning the chamber ]\``).then((msg) => {
-      for (let i = 0; i < array.length; i++) {
+      for (let i = 0; i < newpps.length; i++) {
         setTimeout(() => {
-          if (array[i].id === unlucky.id) {
-            unlucky.timeout(ms(args[0] || "1m"), "roulette");
+          if (newpps[i].id === unlucky.id) {
+            unlucky.timeout(ms(args[0]), "roulette");
     
             message.reply(
               `> **<@!${unlucky.user.id}>** got \`UNLUCKY\` and muted for ${
-                args[0] || "1m"
+                args[0]
               } :rofl:`
             );
 
             return msg.edit(
-              `Gun passed to <@!${array[i].user.id}>\n\`[ Gun shot sounds ]\``
+              `Gun passed to <@!${newpps[i].user.id}>\n\`[ Gun shot sounds ]\``
             );
           }
-          msg.edit(`Gun passed to <@!${array[i].user.id}>\n\`[ Empty trigger pulled ]\``);
-        }, 1000 * i);
+          msg.edit(`Gun passed to <@!${newpps[i].user.id}>\n\`[ Empty trigger pulled ]\``);
+        }, 2000 * i);
       }
     });
   },
---
+++ commands/mod/say.js
@@ -0,0 +1,17 @@
+module.exports = {
+  name: "say",
+  aliases: ["speak"],
+  description: "Make the bot say something",
+  usage: "[channel] <message>",
+  UserPerms: ["MANAGE_MESSAGES"],
+  run: async (client, message, args) => {
+    var channel = message.mentions.channels.first();
+    if (!channel) channel = message.channel;
+
+    var msg = message.channel.id == channel.id ? args.join(" ") : args.slice(1).join(" ");
+    if (!msg) return message.reply("Please specify a message");
+
+    channel.send(msg);
+    message.delete();
+  },
+};
