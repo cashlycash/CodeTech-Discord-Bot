0439fb4807aaf277a3c192c347bd4e9f06f9219e
Arnav Gupta <arnavgupta-11408@bvniketan.onmicrosoft.com>
2024-04-27 22:24:46 +0530

    fix events no response for no events

--- SlashCommands/events/event.js
+++ SlashCommands/events/event.js
@@ -1,38 +1,50 @@
 const {
-	MessageEmbed,
-	MessageSelectMenu,
-	MessageActionRow
-} = require('discord.js');
+  MessageEmbed,
+  MessageSelectMenu,
+  MessageActionRow,
+} = require("discord.js");
 
 const db = require(process.cwd() + "/database.js");
 
 module.exports = {
   name: "events",
   description: "Fetch all active events",
+  np: true,
   run: async (client, interaction) => {
-    
-		const events = await db.get('events')
-		const emb = new MessageEmbed()
-			.setTitle('Events -')
-			.setDescription('Please browse thru the events and click on any to get more details about it')
-			.setColor('BLUE')
+    const events = await db.get("events");
 
-		const row = new MessageActionRow()
-			.setComponents(
-				new MessageSelectMenu()
-					.setCustomId('events')
-					.setPlaceholder('Click to browse')
-					.addOptions(
-						events.map((event, index) => {
-							return {
-								label: event.name,
-								description: `Hosted By - ${event.host}`,
-								value: index.toString(),
-							}
-						})
-					)
-			)
+    if (!events) {
+      return interaction.reply({
+        content: "No events found",
+        ephemeral: true,
+      });
+    }
 
-		interaction.followUp({ embeds: [emb], components: [row] })
-  }
-}
+    const emb = new MessageEmbed()
+      .setTitle("Events:")
+      .setDescription(
+        "Please browse thru the events and click on any to get more details about it"
+      )
+      .setColor("BLUE");
+
+    const row = new MessageActionRow().setComponents(
+      new MessageSelectMenu()
+        .setCustomId("events")
+        .setPlaceholder("Click to browse")
+        .addOptions(
+          events.map((event, index) => {
+            return {
+              label: event.name,
+              description: `Hosted By - ${event.host}`,
+              value: index.toString(),
+            };
+          })
+        )
+    );
+
+    interaction.reply({
+      embeds: [emb],
+      components: [row],
+    });
+  },
+};
